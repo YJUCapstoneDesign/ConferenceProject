6b62d0eccfab34175824e8b31879e98933d0169b
Deepjune <84489820+Deepjune@users.noreply.github.com>
2024-01-19 15:10:20 +0900

    [F][feat#0006] : Modify Code

--- FrontEnd/conference-project/src/App.js
+++ FrontEnd/conference-project/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Component/Navbar';
 import HomeContent from './Component/Home';
 import AboutContent from './Component/About';
 import ServiceContent from './Component/Service';
 import NoticeContent from './Component/Notice';
+import Nav from './Component/NavBar';
 
 function App() {
   return (
--- FrontEnd/conference-project/src/Component/NavBar.js
+++ FrontEnd/conference-project/src/Component/NavBar.js
@@ -1,26 +1,43 @@
-import {Link} from 'react-router-dom';
-import React from 'react';
-import './Nav.css';
+import { Link } from "react-router-dom";
+import React from "react";
 
 function Nav() {
-    return(
-        <div>
-            <div className='navWrap'>
-                <div className='navLogo'>
-                    Zoom
-                </div>
-                <div className='menubar'>
-                    <Link className="navbarMenu" to={'/'}>HOME</Link>
-                    <Link className="navbarMenu" to={'/About'}>ABOUT</Link>
-                    <Link className="navbarMenu" to={'/Service'}>SERVICES</Link>
-                    <Link className="navbarMenu" to={'/Notice'}>NOTICE</Link>
-                    <Link className='navbarButton' to={'https://www.naver.com'}>
-                        <div className='Button'>LOGIN</div>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
-} 
+  return (
+    <div class="shadow bg-white">
+      <div class="h-16 mx-auto px-5 flex items-center justify-between">
+        <a class="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">
+          Logo
+        </a>
+        <ul class="flex items-center gap-5">
+          <li>
+            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+              SERVICES
+            </Link>
+          </li>
+          <li>
+            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+              NOTICE
+            </Link>
+          </li>
+          <li>
+            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+              LOGIN
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Nav;

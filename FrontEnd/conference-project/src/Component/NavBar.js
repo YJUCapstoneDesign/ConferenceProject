7b9e13edeeb721f3d6e3ec0279e10d39f2dbe323
Deepjune <84489820+Deepjune@users.noreply.github.com>
2024-01-19 16:21:07 +0900

    [F][feat#0008] : navBar complete

--- FrontEnd/conference-project/src/Component/NavBar.js
+++ FrontEnd/conference-project/src/Component/NavBar.js
@@ -1,36 +1,43 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import paths from "./paths";
 
 function Nav() {
+  const linkAttr = {
+    color: "hover:text-cyan-500 text-indigo-700 transition-colors font-black",
+  };
+
+  const navItems = [
+    { text: "HOME", path: paths.home },
+    { text: "ABOUT", path: paths.about },
+    { text: "SERVICE", path: paths.service },
+    { text: "NOTICE", path: paths.notice },
+  ];
+
   return (
-    <div class="shadow bg-white">
-      <div class="h-16 mx-auto px-5 flex items-center justify-between">
-        <a class="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">
-          Logo
-        </a>
-        <ul class="flex items-center gap-5">
+    <div className="shadow bg-white">
+      <div className="h-16 mx-auto px-5 flex items-center justify-between">
+        <Link
+          className="text-3xl cursor-pointer text-indigo-700 font-black"
+          to={paths.home}
+        >
+          박운섭
+        </Link>
+
+        <ul className="flex items-center gap-5">
+          {navItems.map((item) => (
+            <li key={item.text}>
+              <Link className={`${linkAttr.color}`} to={item.path}>
+                {item.text}
+              </Link>
+            </li>
+          ))}
           <li>
-            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
-              SERVICES
-            </Link>
-          </li>
-          <li>
-            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
-              NOTICE
-            </Link>
-          </li>
-          <li>
-            <Link class="hover:text-cyan-500 transition-colors" to={"/"}>
+            <Link
+              className="font-black text-white bg-indigo-700 rounded-xl"
+              to={paths.login}
+              style={{ padding: "0.5rem 1rem" }}
+            >
               LOGIN
             </Link>
           </li>

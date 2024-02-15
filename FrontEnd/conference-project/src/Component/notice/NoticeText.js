a129954c3f2ccf0dca20e2eae13f7af6c8e17713
Deepjune <84489820+Deepjune@users.noreply.github.com>
2024-02-15 17:48:03 +0900

    [F][feat#0021] : add image

--- FrontEnd/conference-project/src/Component/notice/NoticeText.js
+++ FrontEnd/conference-project/src/Component/notice/NoticeText.js
@@ -1,26 +1,26 @@
 /* eslint-disable */
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function NoticeTextContent(props) {
-    return (
-        <React.Fragment>
-            <div className="max-w-lg md:mx-12 md:order-2">
-                <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">
-                    {props.title}
-                </h1>
-                <p className="mt-4 text-gray-600 dark:text-gray-300">{props.content}</p>
-                <div className="mt-6">
-                    <a
-                        href="#"
-                        className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400"
-                    >
-                        button
-                    </a>
-                </div>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className="max-w-lg md:mx-12 md:order-2">
+        <h1 className="text-3xl font-medium tracking-wide text-white dark:text-gray-300 md:text-4xl">
+          {props.title}
+        </h1>
+        <p className="mt-4 text-gray-600 dark:text-gray-300">{props.content}</p>
+        <div className="mt-6">
+          <a
+            href="#"
+            className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400"
+          >
+            button
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default NoticeTextContent;

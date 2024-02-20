bd762704de6ad843989a83269ccbba4b73e4b44a
Deepjune <wjdwnsrn9021@naver.com>
2024-02-20 16:22:47 +0900

    [F][feat#0026] : Modify Code

--- FrontEnd/conference-project/src/Component/notice-in/NoticePage.js
+++ FrontEnd/conference-project/src/Component/notice-in/NoticePage.js
@@ -218,7 +218,7 @@ function NoticePageContents() {
 
                             <a
                                 href="/"
-                                class="transition-all  duration-1000 bg-white hover:bg-[#0094C6]  hover:shadow-xl m-2 p-4 relative z-40 group  "
+                                class="transition-all  duration-1000 bg-white hover:bg-[#d8dddf]  hover:shadow-xl m-2 p-4 relative z-40 group  "
                             >
                                 <div class=" absolute  bg-blue-500/50 top-0 left-0 w-24 h-1 z-30  transition-all duration-200   group-hover:bg-white group-hover:w-1/2  "></div>
                                 <div class="py-2 px-9 relative  ">

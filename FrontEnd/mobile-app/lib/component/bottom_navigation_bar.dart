63ba70c7f297f94cbd770c1ec1b6643e13d08207
Deepjune <84489820+Deepjune@users.noreply.github.com>
2024-03-19 19:13:34 +0900

    Modyfy Mobile App

---
+++ FrontEnd/mobile-app/lib/component/bottom_navigation_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyCustomBottomNavigationBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.videocam),
+              label: 'VideoCam',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'alarm',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'MyPage',
+            ),
+          ]
+      ),
+    );
+  }
+}
---
+++ FrontEnd/mobile-app/lib/screen/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0B0B5A),
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}

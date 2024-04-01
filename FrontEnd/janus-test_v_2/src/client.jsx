ac99210daa40b3ecf923d28d67b48a72bbacc0da
codevarty <hisu7789@g.yju.ac.kr>
2024-04-01 19:20:20 +0900

    create janus file with https in react

---
+++ FrontEnd/janus-test_v_2/src/client.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+// react 18 + Not supported ReactDom.render
+// createRoot use for react 18
+createRoot(document.getElementById('root')).render(
+    <App />
+  );

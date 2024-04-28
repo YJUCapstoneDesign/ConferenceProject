65592544fa9230ed29b534600816db739bec3fef
sihyeon0701 <jinwoong5541@naver.com>
2024-04-28 20:00:32 +0900

    webpack error modified

--- FrontEnd/janus-test_v_2/src/client.jsx
+++ FrontEnd/janus-test_v_2/src/client.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./client.css";
+import "./tailwind.css";
 
 // react 18 + Not supported ReactDom.render
 // createRoot use for react 18
--- FrontEnd/janus-test_v_2/webpack.config.js
+++ FrontEnd/janus-test_v_2/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const fs = require('fs');
@@ -20,10 +19,8 @@ module.exports = {
         }
       },
       {
-        test: /\.(css|scss)$/,
-        // use: ['style-loader', 'css-loader'],
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        exclude: /node_modules/,
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: require.resolve('janus-gateway'),
@@ -45,9 +42,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
-    new MiniCssExtractPlugin({
-      filename: 'app.css'
-    }),
+    // new MiniCssExtractPlugin({
+    //   filename: 'app.css'
+    // }),
     new webpack.ProvidePlugin({
       adapter: ['webrtc-adapter', 'default']
     }),

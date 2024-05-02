5eee9cb4f7d6289a60645d8e9c28ba74d7191c7d
codevarty <hisu7789@gmail.com>
2024-05-02 10:53:38 +0900

    Modify Code websocket controller

--- BackEnd/broadcast/src/main/java/team/broadcast/domain/mindmap/controller/WebSocketMindMapController.java
+++ BackEnd/broadcast/src/main/java/team/broadcast/domain/mindmap/controller/WebSocketMindMapController.java
@@ -1,6 +1,7 @@
 package team.broadcast.domain.mindmap.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,11 @@ import team.broadcast.domain.mindmap.dto.UpdateMindMapDto;
 public class WebSocketMindMapController {
 
     // 실시간으로 클라이언트에 작업하는 코드가 전달이 된다.
-    @MessageMapping("/ws/mind-map")
-    @SendTo("/topic/update")
-    public UpdateMindMapDto update(UpdateMindMapDto updateMindMap) {
+    // 여러 개의 화상회의에서 작업을 하기 위해 id를 주었다.
+    @MessageMapping("/ws/mind-map/{id}")
+    @SendTo("/topic/update/{id}")
+    public UpdateMindMapDto update(@DestinationVariable String id, UpdateMindMapDto updateMindMap) {
+        log.info("mind map data {}", updateMindMap);
         return updateMindMap;
     }
 }

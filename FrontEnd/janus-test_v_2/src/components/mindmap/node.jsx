4d0c3e0d44b94ddb66b9701591f15fd30240f5bf
Deepjune <wjdwnsrn9021@naver.com>
2024-05-15 01:17:36 +0900

    Modify Code

--- FrontEnd/janus-test_v_2/src/components/mindmap/node.jsx
+++ FrontEnd/janus-test_v_2/src/components/mindmap/node.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -9,24 +9,39 @@ import ReactFlow, {
 } from 'reactflow';
  
 import 'reactflow/dist/style.css';
- 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
+
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
  
-export default function node() {
+let initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  ];
+
+export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+
+    useEffect(() => {
+        console.log(nodes)
+        return () => {
+            console.log('finish')
+        }
+    },[nodes])
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
- 
+
+  const addNode = useCallback(
+    () => {
+        setNodes((initialNodes)=>[...initialNodes, { id: `${initialNodes.length + 1}`, position: { x: 10, y: 200 }, data: { label: `${initialNodes.length + 1}` } }])
+    }
+);
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+        <button onClick={addNode}>추가 버튼</button>
       <ReactFlow
         nodes={nodes}
         edges={edges}

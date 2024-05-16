50bada213b788cc4ca0c2de281abd24f9f642b40
Deepjune <wjdwnsrn9021@naver.com>
2024-05-16 16:26:30 +0900

    Modify Code

--- FrontEnd/janus-test_v_2/src/App.jsx
+++ FrontEnd/janus-test_v_2/src/App.jsx
@@ -1,6 +1,7 @@
 const React = require("react");
 const { default: VideoPage } = require("./components/pages/VideoPage");
 const { default: MindmapPage } = require("./components/mindmap/node");
+const { default: BoardPage } = require("./components/whiteboard/board");
 const { Routes, Route } = require("react-router-dom");
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<VideoPage />} />
       <Route path="/mindmap" element={<MindmapPage />} />
+      <Route path="/whiteboard" element={<BoardPage />} />
     </Routes>
   );
 };

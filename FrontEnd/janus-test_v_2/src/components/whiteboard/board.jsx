b63d49b33b455d1a0e5cba86d2853d9d48fb37f4
Deepjune <wjdwnsrn9021@naver.com>
2024-05-25 02:04:24 +0900

    Modify Code

--- FrontEnd/janus-test_v_2/src/components/whiteboard/board.jsx
+++ FrontEnd/janus-test_v_2/src/components/whiteboard/board.jsx
@@ -1,43 +1,25 @@
-import { Tldraw } from 'tldraw';
-import 'tldraw/tldraw.css';
-import { useRef, useEffect, useState } from 'react';
-import React from 'react';
+import { useEditor, Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+import React, { useEffect, useState } from 'react'
 
-function RealtimeTldraw() {
-  const tldrawRef = useRef(null);
-  const [socket, setSocket] = useState(null);
+export default function Board() {
+	const editor = useEditor()
 
-  let ws = useRef(null);
-
-  useEffect(() => {
-    // 웹소켓 연결
-    ws.current = new WebSocket("ws://localhost:8080/app");
-
-    ws.onopen = () => {
-      console.log("WebSocket Connected");
-    };
-
-    ws.current.onmessage = (message) => {
-      setSocket(message.data)
-    };
-    return () => {
-      // 컴포넌트가 unmount될 때 웹소켓 연결 종료
-      ws.close();
-    };
-  }, []);
-
-  const handleTldrawChange = () => {
-    // Tldraw 상태 변경 시 웹소켓을 통해 서버로 전송
-    const data = tldrawRef.current.value;
-    console.log(data)
-    socket.send(JSON.stringify(data));
-  };
-
-  return (
-    <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw ref={tldrawRef} onChange={handleTldrawChange} />
-    </div>
-  );
+	console.log(editor)
+	
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw 
+				editor={editor}
+				onReady={() => {
+					editor.setCanvas({
+						background: '#fff',
+						gridSize: 10,
+						gridColor: '#ddd',
+						gridOpacity: 0.5,
+					})
+				}}
+			/>
+		</div>
+	)
 }
-
-export default RealtimeTldraw;

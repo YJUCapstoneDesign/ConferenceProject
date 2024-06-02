1ddf988e2fc911eb280275cbc31c26ed1f6fad38
codevarty <hisu7789@gmail.com>
2024-06-02 21:57:00 +0900

    append Hat Handler

--- BackEnd/broadcast/src/main/java/team/broadcast/global/config/WebSocketConfig.java
+++ BackEnd/broadcast/src/main/java/team/broadcast/global/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import team.broadcast.global.handler.HatHandler;
 import team.broadcast.global.handler.MindMapHandler;
 
 @Configuration
@@ -13,10 +14,16 @@ import team.broadcast.global.handler.MindMapHandler;
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final MindMapHandler mindMapHandler;
+    private final HatHandler hatHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // MindMap 소켓 주소 /app
         registry.addHandler(mindMapHandler, "/app")
+                .setAllowedOrigins("*");
+
+        // 생각 모자 소켓 주소 /hat
+        registry.addHandler(hatHandler, "/hat")
                 .setAllowedOrigins("*");
     }
 }
---
+++ BackEnd/broadcast/src/main/java/team/broadcast/global/handler/HatHandler.java
@@ -0,0 +1,55 @@
+package team.broadcast.global.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@Component
+public class HatHandler extends TextWebSocketHandler {
+
+    // 현재 연결된 유저 세션들
+    private final Set<WebSocketSession> sessions = new HashSet<>();
+
+    // 소켓 연결 뒤
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("Connected to {}", session.getId());
+        sessions.add(session);
+    }
+
+    // 소켓 종료
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("Disconnected from {}", session.getId());
+        sessions.remove(session);
+    }
+
+    // 소켓 데이터 전송 부분
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload(); // 전송한 데이터 부분
+        log.info("received message: {}", payload);
+
+        // 채팅 부분 코드를 아래에 작성을 해봅시다.
+        sessions.forEach(s -> {
+            // 아이디가 다른 사람에게 채팅 메시지를 보낸다.
+            if (s.getId().equals(session.getId())) {
+                return;
+            }
+
+            try {
+                s.sendMessage(new TextMessage(payload));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}

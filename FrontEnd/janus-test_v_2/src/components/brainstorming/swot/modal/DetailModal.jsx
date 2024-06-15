6533e958f133cd1a4ad400d7af510cd804544bff
sihyeon0701 <jinwoong5541@naver.com>
2024-06-15 23:30:18 +0900

    modify code

--- FrontEnd/janus-test_v_2/src/components/brainstorming/swot/modal/DetailModal.jsx
+++ FrontEnd/janus-test_v_2/src/components/brainstorming/swot/modal/DetailModal.jsx
@@ -18,26 +18,25 @@ const DetailModal = ({ onClose, title, content, area, updateTile, deleteTile }) 
   return (
     <ModalPortal>
       <div className="h-screen w-full fixed bg-[#00000066] left-0 top-0 flex justify-center items-center bg-main-black bg-opacity-70">
-        <div className="container w-[300px] h-[300px] bg-white border-2">
+        <div className="container w-[300px] h-[325px] bg-white border-2 rounded-lg p-5">
           <div className="w-full h-full">
-            <h1 className="text-2xl font-bold text-center text-black">Detail</h1>
-            <div className="p-4">
+            <h1 className="text-2xl font-bold text-center text-gray-500">Detail</h1>
+            <div className="p-1">
               <input
-                className="w-full mb-2 p-2 border"
+                className="block w-full h-9 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 "
                 value={editTitle}
                 onChange={(e) => setEditTitle(e.target.value)}
               />
               <textarea
-                className="w-full mb-2 p-2 border"
+                className="mt-3 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                 value={editContent}
                 onChange={(e) => setEditContent(e.target.value)}
-              />
-              <p className="text-center text-black">Area: {area}</p>
+              />  
             </div>
           </div>
-          <button onClick={handleUpdate} className="w-full py-2 bg-green-400">Update</button>
-          <button onClick={handleDelete} className="w-full py-2 bg-red-400">Delete</button>
-          <button onClick={onClose} className="w-full py-2 bg-blue-400">Close</button>
+          <button onClick={handleUpdate} className="flex-1 text-white bg-gradient-to-r from-indigo-500 to-indigo-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 shadow-lg shadow-indigo-500/50 dark:shadow-lg  font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-3">Update</button>
+          <button onClick={handleDelete} className="flex-1 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 shadow-lg shadow-red-500/50 dark:shadow-lg  font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-3">Delete</button>
+          <button onClick={onClose} className="flex-1 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 shadow-lg shadow-red-500/50 dark:shadow-lg  font-medium rounded-lg text-sm px-5 py-2.5 text-center">Close</button>
         </div>
       </div>
     </ModalPortal>
--- FrontEnd/janus-test_v_2/src/components/page-in/ProfileEdit.js
+++ FrontEnd/janus-test_v_2/src/components/page-in/ProfileEdit.js
@@ -37,10 +37,11 @@ function ProfileEdit() {
     <div>
       {/* Below is Profile Edit */}
       <div className="max-w-lg mx-auto mt-48 bg-white dark:bg-gray-800 rounded-lg shadow-md px-8 py-10 flex flex-col items-center">
-        <div className="w-full flex flex-col gap-4 mt-20">
+        <div className="w-full flex flex-col gap-4">
           <NavIn />
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div className="flex items-start flex-col justify-start">
+            <h2 class="text-2xl font-semibold text-black mb-10">Edit Profile!</h2>
               <label htmlFor="username" className="text-sm text-gray-700 dark:text-gray-200 mr-2">Username:</label>
               <input
                 type="text"

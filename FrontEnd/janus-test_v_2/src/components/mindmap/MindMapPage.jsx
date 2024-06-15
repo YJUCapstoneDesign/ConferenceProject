b8af58b0eea0790d804a74e86a2acace3b347abd
JinHyeok0519 <125631311+JinHyeok0519@users.noreply.github.com>
2024-06-15 17:57:14 +0900

    modify code

--- FrontEnd/janus-test_v_2/src/components/mindmap/MindMapPage.jsx
+++ FrontEnd/janus-test_v_2/src/components/mindmap/MindMapPage.jsx
@@ -12,42 +12,34 @@ import { uploadToS3 } from './FileUploadDownload';
 import { listUploadedFiles, downloadFileFromS3 } from './FileUploadDownload'; 
 import { useParams } from 'react-router-dom';
 
-let initialNodes = [
-  
-];
-
 const baseURL = process.env.REACT_WEBSOCKET_SERVER;
 
-let initialEdges = [
-
-];
+const initialNodes = [];
+const initialEdges = [];
 
 export default function MindMapPage() {
 
-  const {teamNumber} = useParams();
+  const { teamNumber } = useParams();
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [socketData, setSocketData] = useState();
+  const [fileList, setFileList] = useState([]);
   const [selectedNodes, setSelectedNodes] = useState([]);
   const [selectedEdges, setSelectedEdges] = useState([]);
-  const [fileList, setFileList] = useState([]);
 
-
-  const ws = useRef(null); // 웹소켓 연결을 위한 ref
-
+  const ws = useRef(null);
 
   const handleSaveClick = async () => {
     const mindMapData = {
-      nodes: initialNodes,
-      edges: initialEdges,
+      nodes,
+      edges,
     };
 
     const mindMapDataJson = JSON.stringify(mindMapData);
 
-    const uploadResult = await uploadToS3(mindMapDataJson,teamNumber);
+    const uploadResult = await uploadToS3(mindMapDataJson, teamNumber);
 
-    setFileList([]); //리스트 초기화
+    setFileList([]);
 
     if (uploadResult.success) {
       alert('파일 저장 성공!');
@@ -58,10 +50,7 @@ export default function MindMapPage() {
 
   const handleLoadData = async () => {
     try {
-      // 파일 목록을 받아옴
       const files = await listUploadedFiles(teamNumber);
-      
-      // 파일 목록 상태 업데이트
       setFileList(files);
     } catch (error) {
       console.error('데이터 로드 실패:', error);
@@ -73,52 +62,41 @@ export default function MindMapPage() {
       const fileContent = await downloadFileFromS3(fileName);
       const fileContentString = new TextDecoder().decode(fileContent.value);
       const { nodes: newNodes, edges: newEdges } = JSON.parse(fileContentString);
-      initialNodes = newNodes;
-      initialEdges = newEdges;
-  
+
+      setNodes(newNodes);
+      setEdges(newEdges);
+
       const newData = {
-        node: initialNodes,
-        edge: initialEdges
+        node: newNodes,
+        edge: newEdges
       };
-  
+
       sendWebSocketData(newData);
-      setFileList([]); //리스트 초기화
+      setFileList([]);
     } catch (error) {
       console.error('파일 적용 실패:', error);
     }
   };
-  
 
   useEffect(() => {
     ws.current = new WebSocket(`${baseURL}/app`);
     console.log("웹소켓 연결됨");
 
     ws.current.onopen = () => {
-      ws.current.send(
-        JSON.stringify(
-          { id: parseInt(teamNumber), type: "ENTER" }
-        )
-      );
+      ws.current.send(JSON.stringify({ id: parseInt(teamNumber), type: "ENTER" }));
     };
 
-    ws.current.onmessage = (message) => { // 서버에서 메시지가 오면 실행
-      console.log("message: ", message.data);
-      setSocketData(message.data);
+    ws.current.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      console.log("message: ", data);
+      setNodes(data.node);
+      setEdges(data.edge);
     };
 
     return () => {
       ws.current.close();
     };
-  }, []);
-
-  useEffect(() => {
-    if (socketData !== undefined) {
-      const parsedSocketData = JSON.parse(socketData);
-      console.log("parsedSocketData: ", parsedSocketData);
-      setNodes(parsedSocketData.node);
-      setEdges(parsedSocketData.edge);
-    }
-  }, [socketData]);
+  }, [teamNumber]);
 
   const sendWebSocketData = useCallback((data) => {
     console.log("sendWebSocketData: ", data);
@@ -126,7 +104,7 @@ export default function MindMapPage() {
       id: parseInt(teamNumber),
       type: "MSG",
       data,
-    }
+    };
     if (ws.current && ws.current.readyState === WebSocket.OPEN) {
       ws.current.send(JSON.stringify(sendData));
     } else {
@@ -134,7 +112,7 @@ export default function MindMapPage() {
         ws.current.send(JSON.stringify(sendData));
       };
     }
-  }, []);
+  }, [teamNumber]);
 
   const addNode = useCallback(() => {
     const newNode = {
@@ -143,7 +121,7 @@ export default function MindMapPage() {
       data: { label: "New Node" },
     };
     const newNodes = [...nodes, newNode];
-    initialNodes = newNodes;
+    setNodes(newNodes);
 
     const newData = {
       node: newNodes,
@@ -151,125 +129,90 @@ export default function MindMapPage() {
     };
 
     sendWebSocketData(newData);
-  }, [nodes, sendWebSocketData]);
+  }, [nodes, edges, sendWebSocketData]);
 
-  const onSelectionDragStop = (e, nodes) => {
-    nodes.forEach((node) => {
-      const index = initialNodes.findIndex(item => item.id === node.id);
-      if (index !== -1) {
-        initialNodes[index].position = node.position;
-      }
-    });
+  const onNodeDragStop = useCallback((e, dragNode) => {
+    const updatedNodes = nodes.map(node => node.id === dragNode.id ? dragNode : node);
+    setNodes(updatedNodes);
 
     const newData = {
-      node: initialNodes,
-      edge: initialEdges
+      node: updatedNodes,
+      edge: edges
     };
 
     sendWebSocketData(newData);
-  };
+  }, [nodes, edges, sendWebSocketData]);
 
-  const onNodeDragStop = (e, dragNode, nodes) => {
-    nodes.forEach((node) => {
-      const index = initialNodes.findIndex(item => item.id === node.id);
-      if (index !== -1) {
-        initialNodes[index].position = node.position;
-      }
-    });
+  const onNodesDelete = useCallback(() => {
+    const updatedNodes = nodes.filter(node => !selectedNodes.some(selected => selected.id === node.id));
+    const updatedEdges = edges.filter(edge => !selectedNodes.some(selected => selected.id === edge.source || selected.id === edge.target));
+    setNodes(updatedNodes);
+    setEdges(updatedEdges);
 
     const newData = {
-      node: initialNodes,
-      edge: initialEdges
+      node: updatedNodes,
+      edge: updatedEdges
     };
 
     sendWebSocketData(newData);
-  };
+  }, [nodes, edges, selectedNodes, sendWebSocketData]);
 
-  const onNodesDelete = () => {
-    const newNodes = nodes.filter(node => node.id !== selectedNodes.id);
-    initialNodes = newNodes;
-
-    const newEdges = edges.filter(edge => edge.source !== selectedNodes.id && edge.target !== selectedNodes.id);
-    initialEdges = newEdges;
-
-    const newData = {
-      node: initialNodes,
-      edge: initialEdges
-    };
-
-    sendWebSocketData(newData);
-  };
-
-  const onEdgesConnect = (params) => {
+  const onEdgesConnect = useCallback((params) => {
     const newEdge = {
       id: `e${params.source}-${params.target}`,
       source: params.source,
       target: params.target,
     };
-    const newEdges = [...edges, newEdge];
-    initialEdges = newEdges;
+    const updatedEdges = [...edges, newEdge];
+    setEdges(updatedEdges);
 
     const newData = {
-      node: initialNodes,
-      edge: newEdges
+      node: nodes,
+      edge: updatedEdges
     };
 
     sendWebSocketData(newData);
-  };
+  }, [nodes, edges, sendWebSocketData]);
 
-  const onEdgesDelete = () => {
-    const newEdges = edges.filter(edge => edge.id !== selectedEdges.id);
-    initialEdges = newEdges;
+  const onEdgesDelete = useCallback(() => {
+    const updatedEdges = edges.filter(edge => !selectedEdges.some(selected => selected.id === edge.id));
+    setEdges(updatedEdges);
 
     const newData = {
-      node: initialNodes,
-      edge: newEdges
+      node: nodes,
+      edge: updatedEdges
     };
 
     sendWebSocketData(newData);
-  };
+  }, [nodes, edges, selectedEdges, sendWebSocketData]);
 
-  const onNodeDoubleClick = (e) => {
-    const newLabel = prompt("새로운 라벨을 입력하세요.");
-    const newNodes = nodes.map((item) => {
-      if (item.id === selectedNodes.id) {
-        item.data.label = newLabel;
-      }
-      return item;
-    });
-    initialNodes = newNodes;
+  const onNodeDoubleClick = useCallback((e, node) => {
+    const newLabel = prompt("새로운 라벨을 입력하세요.", node.data.label);
+    const updatedNodes = nodes.map(item => item.id === node.id ? { ...item, data: { ...item.data, label: newLabel } } : item);
+    setNodes(updatedNodes);
 
     const newData = {
-      node: newNodes,
-      edge: initialEdges
+      node: updatedNodes,
+      edge: edges
     };
 
     sendWebSocketData(newData);
-  };
-
-  const onSelectNodes = (e, nodes) => {
-    setSelectedNodes(nodes);
-  };
-
-  const onSelectEdges = (e, edges) => {
-    setSelectedEdges(edges);
-  };
+  }, [nodes, edges, sendWebSocketData]);
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <div className="inline-flex shadow-sm rounded-sm mt-1 ml-1" role="group">
-      <button onClick={addNode} className='px-4 py-2 text-sm font-semibold text-gray-900 rounded-s-lg bg-transparent border hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
-        Add
-      </button>
-      <button onClick={handleSaveClick} className='px-4 py-2 text-sm font-semibold text-gray-900 bg-transparent border-t border-b hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
-        Save
-      </button>
-      <button onClick={handleLoadData} className='px-4 py-2 text-sm font-semibold text-gray-900 rounded-e-lg bg-transparent border hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
-        Load
-      </button>
+        <button onClick={addNode} className='px-4 py-2 text-sm font-semibold text-gray-900 rounded-s-lg bg-transparent border hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
+          Add
+        </button>
+        <button onClick={handleSaveClick} className='px-4 py-2 text-sm font-semibold text-gray-900 bg-transparent border-t border-b hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
+          Save
+        </button>
+        <button onClick={handleLoadData} className='px-4 py-2 text-sm font-semibold text-gray-900 rounded-e-lg bg-transparent border hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700 transition-colors duration-500 ease-in-out'>
+          Load
+        </button>
       </div>
-      {/* 파일 목록을 표시하는 부분 */} 
-      {fileList && fileList.length > 0 && (
+      {fileList.length > 0 && (
         <div>
           <h2>파일 목록</h2>
           <ul>
@@ -282,7 +225,6 @@ export default function MindMapPage() {
           </ul>
         </div>
       )}
-      
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -290,13 +232,11 @@ export default function MindMapPage() {
         onEdgesChange={onEdgesChange}
         onNodeDragStop={onNodeDragStop}
         onNodesDelete={onNodesDelete}
-        onNodeClick={onSelectNodes}
+        onNodeClick={(_, node) => setSelectedNodes([node])}
         onNodeDoubleClick={onNodeDoubleClick}
         onConnect={onEdgesConnect}
-        onEdgeClick={onSelectEdges}
+        onEdgeClick={(_, edge) => setSelectedEdges([edge])}
         onEdgesDelete={onEdgesDelete}
-        onSelectionChange={() => {}}
-        onSelectionDragStop={onSelectionDragStop}
       >
         <Controls />
         <MiniMap />

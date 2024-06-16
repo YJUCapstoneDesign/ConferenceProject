32ea977bff1e188347e6544c39add5bef6595443
sihyeon0701 <jinwoong5541@naver.com>
2024-06-17 03:05:32 +0900

    modify code

--- FrontEnd/janus-test_v_2/src/components/brainstorming/swot/modal/AddModal.jsx
+++ FrontEnd/janus-test_v_2/src/components/brainstorming/swot/modal/AddModal.jsx
@@ -31,21 +31,21 @@ const AddModal = ({ onClose, setDataList, sendWebSocketData }) => {
       <div className="h-screen w-full fixed bg-[#00000066] left-0 top-0 flex justify-center items-center bg-main-black bg-opacity-70">
         <div className="container w-[300px] h-auto bg-white border-2 rounded-lg p-5">
           <form onSubmit={submitHandler}>
-            <select name="area" id="area" className="py-2 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-600 block w-full p-2.5 font-bold dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-600 dark:focus:border-indigo-600">
+            <select name="area" id="area" className="py-2 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-600 block w-full p-2.5 font-bold">
               <option value={1}>STRENGTHS</option>
               <option value={2}>WEAKNESSES</option>
               <option value={3}>OPPORTUNITIES</option>
               <option value={4}>THREATS</option>
             </select>
             <input 
-              className="block w-full h-9 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-2" 
+              className="block w-full h-9 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 mt-2" 
               type="text" 
               placeholder="Title" 
               value={title}
               onChange={(e) => setTile(e.target.value)}
             />
             <textarea 
-              className="mt-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="mt-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
               name="content" 
               placeholder="Detailed technical description" 
               id="content" 
@@ -55,8 +55,8 @@ const AddModal = ({ onClose, setDataList, sendWebSocketData }) => {
               onChange={(e) => setContent(e.target.value)}
             />
             <div className="flex justify-between gap-x-4 mt-4">
-              <button onClick={onClose} className="flex-1 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Cancel</button>
-              <button type="submit" className="flex-1 text-white bg-gradient-to-r from-indigo-500 to-indigo-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 shadow-lg shadow-indigo-500/50 dark:shadow-lg dark:shadow-indigo-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save</button>
+              <button onClick={onClose} className="flex-1 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300  shadow-red-500/50 dark:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center">Cancel</button>
+              <button type="submit" className="flex-1 text-white bg-gradient-to-r from-indigo-500 to-indigo-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300  shadow-lg shadow-indigo-500/50 dark:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save</button>
             </div>
           </form>
         </div>  
--- FrontEnd/janus-test_v_2/src/components/page-in/ProfileEdit.js
+++ FrontEnd/janus-test_v_2/src/components/page-in/ProfileEdit.js
@@ -36,32 +36,32 @@ function ProfileEdit() {
   return (
     <div>
       {/* Below is Profile Edit */}
-      <div className="max-w-lg mx-auto mt-48 bg-white dark:bg-gray-800 rounded-lg shadow-md px-8 py-10 flex flex-col items-center">
+      <div className="max-w-lg mx-auto mt-48 bg-white rounded-lg shadow-md px-8 py-10 flex flex-col items-center">
         <div className="w-full flex flex-col gap-4">
           <NavIn />
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div className="flex items-start flex-col justify-start">
             <h2 class="text-2xl font-semibold text-black mb-10">Edit Profile!</h2>
-              <label htmlFor="username" className="text-sm text-gray-700 dark:text-gray-200 mr-2">Username:</label>
+              <label htmlFor="username" className="text-sm text-gray-700  mr-2">Username:</label>
               <input
                 type="text"
                 id="username"
                 name="username"
                 value={formData.username}
                 onChange={handleChange}
-                className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="w-full px-3  py-2 rounded-md border border-gray-300focus:outline-none focus:ring-1 focus:ring-blue-500"
               />
             </div>
 
             <div className="flex items-start flex-col justify-start">
-              <label htmlFor="phone" className="text-sm text-gray-700 dark:text-gray-200 mr-2">Phone:</label>
+              <label htmlFor="phone" className="text-sm text-gray-700 mr-2">Phone:</label>
               <input
                 type="text"
                 id="phone"
                 name="phone"
                 value={formData.phone}
                 onChange={handleChange}
-                className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
               />
             </div>
 

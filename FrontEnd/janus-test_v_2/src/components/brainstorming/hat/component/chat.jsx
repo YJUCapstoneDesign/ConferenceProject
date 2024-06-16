b33971907e135d500dc14a9e3d74f81ef9598e11
Deepjune <wjdwnsrn9021@naver.com>
2024-06-17 00:15:55 +0900

    Modify Code

--- FrontEnd/janus-test_v_2/src/components/brainstorming/hat/component/chat.jsx
+++ FrontEnd/janus-test_v_2/src/components/brainstorming/hat/component/chat.jsx
@@ -82,7 +82,7 @@ const ChatArea = ({roomId}) => {
               {isOutgoing ? (
                 <OutgoingMessage message={newData} imageUrl="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
               ) : (
-                <IncomingMessage message={message} imageUrl="../images/human.png" />
+                <IncomingMessage message={message} imageUrl="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
               )}
             </div>
           );

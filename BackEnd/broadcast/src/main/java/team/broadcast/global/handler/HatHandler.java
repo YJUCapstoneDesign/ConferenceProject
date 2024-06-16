f2701e0da9d71e0c844fb4f69433ae2d35e256a6
codevarty <hisu7789@gmail.com>
2024-06-16 15:06:31 +0900

    Modify Hat handler code

--- BackEnd/broadcast/src/main/java/team/broadcast/global/handler/HatHandler.java
+++ BackEnd/broadcast/src/main/java/team/broadcast/global/handler/HatHandler.java
@@ -1,11 +1,14 @@
 package team.broadcast.global.handler;
 
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import team.broadcast.global.handler.dto.WebsocketRoom;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -15,24 +18,23 @@ import java.util.Set;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class HatHandler extends TextWebSocketHandler {
-
-    // 현재 연결된 유저 세션들
-    private final Set<WebSocketSession> sessions = new HashSet<>();
-    private final Map<Long, Set<WebSocketSession>> sessionMap = new HashMap<>();
+    private final Gson gson;
+    private final Map<Long, Set<WebSocketSession>> rooms = new HashMap<>();
 
     // 소켓 연결 뒤
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("Connected to {}", session.getId());
-        sessions.add(session);
     }
 
     // 소켓 종료
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info("Disconnected from {}", session.getId());
-        sessions.remove(session);
+        // 세션이 방에 속한 곳 전부 삭제
+        rooms.values().forEach(sess -> sess.remove(session));
     }
 
     // 소켓 데이터 전송 부분
@@ -41,19 +43,28 @@ public class HatHandler extends TextWebSocketHandler {
         String payload = message.getPayload(); // 전송한 데이터 부분
         log.info("received message: {}", payload);
 
-        // 채팅 부분 코드를 아래에 작성을 해봅시다.
-        sessions.forEach(s -> {
-            try {
-                // 같은 사람인 경우
-                if (s.getId().equals(session.getId())) {
-                    s.sendMessage(new TextMessage("[M]" + payload));
-                    return;
-                }
+        Map map = gson.fromJson(payload, Map.class);
 
-                s.sendMessage(new TextMessage(payload));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        log.info("data: {}", map);
+
+        Double id = (Double) map.get("id");
+
+        Long roomId = id.longValue();
+        String data = (String) map.get("data");
+
+        Set<WebSocketSession> webSocketSessions = rooms.computeIfAbsent(roomId, k -> new HashSet<>());
+
+        webSocketSessions.add(session);
+
+        // 메시지 전송하기
+        for (WebSocketSession sess : webSocketSessions) {
+            if (sess.isOpen()) {
+                if (sess.getId().equals(session.getId())) {
+                    sess.sendMessage(new TextMessage("[M]" + data));
+                } else {
+                    sess.sendMessage(new TextMessage(data));
+                }
             }
-        });
+        }
     }
 }
